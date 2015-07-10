feb2942c3fe8b5e0a2a352f484d3fd672e2bf5a5
Nicolas Azrak <azrak.nicolas@gmail.com>
2015-07-10 12:42:12 -0300

    Correccion reducer

--- reduce.py
+++ reduce.py
@@ -1,22 +1,22 @@
 #! /usr/bin/python2
 import sys
- 
-dictionary = {}
- 
-def add_word(token, repetitions):
-    if dictionary.has_key(token):
-        dictionary[token] += int(repetitions)
-    else:
-        dictionary[token] = int(repetitions)
- 
-def print_words():
-    for word in dictionary:
-        sys.stdout.write(word+" " +str(dictionary[word])+"\n")
- 
- 
+
+acumulator = 0
+old_key = None
+
 for token in sys.stdin.readlines():
+
     word = token.split()[0]
-    repetitions = token.split()[1]
-    add_word(word, repetitions)
- 
-print_words()
+    repetitions = int(token.split()[1])
+
+    if old_key is None:
+        old_key = word
+
+    if old_key != word:
+        sys.stdout.write(old_key + " " + str(acumulator) + "\n")
+        acumulator = 0
+        old_key = word
+
+    acumulator += repetitions
+
+sys.stdout.write(old_key + " " + str(acumulator) + "\n")
